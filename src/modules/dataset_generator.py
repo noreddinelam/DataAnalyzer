2df44f7e73d5c428af449c097f5bef7bed408901
Dalil CHABLIS <dalil.chablis@gmail.com>
2022-04-11 01:13:29 +0200

    wip: Dataset generator with selenium

--- src/modules/dataset_generator.py
+++ src/modules/dataset_generator.py
@@ -1,66 +1,117 @@
 import os
+import shutil
+import time
 import requests
-from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 
 GOOGLE_IMAGES_URL = "https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&"
-
-REQUEST_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-    "Accept-Encoding": "none",
-    "Accept-Language": "en-US,en;q=0.8",
-    "Connection": "keep-alive"
-}
 
 DATASET_FOLDER = "../dataset"
 
 
 def main():
-    if not os.path.exists(DATASET_FOLDER):
-        os.mkdir(DATASET_FOLDER)
-    download_images()
+    if os.path.exists(DATASET_FOLDER):
+        shutil.rmtree(DATASET_FOLDER)
+
+    make_dir(DATASET_FOLDER)
+    download_google_images()
 
 
-def download_images():
+def make_dir(path: str):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+# https://selenium-python.readthedocs.io/getting-started.html
+# https://www.geeksforgeeks.org/download-google-image-using-python-and-selenium/
+def download_google_images():
     DATASET_FOLDER_IMAGES = DATASET_FOLDER + "/images"
-    if not os.path.exists(DATASET_FOLDER_IMAGES):
-        os.mkdir(DATASET_FOLDER_IMAGES)
+    make_dir(DATASET_FOLDER_IMAGES)
 
     data = input("What are you looking for ? ")
-    data = data.strip()
     n_images = int(input("How many images do you want ? "))
 
     print("Start searching...")
+    tic = time.perf_counter()
 
     search_url = GOOGLE_IMAGES_URL + "q=" + data
 
-    response = requests.get(search_url, headers=REQUEST_HEADERS)
+    options = Options()
+    options.headless = False
 
-    soup = BeautifulSoup(response.content, "html.parser")
-    img_results = soup.findAll("img", {"class": "yWs4tf"}, limit=n_images)  # soup.findAll("img", {"class": "yWs4tf"})
+    browser = webdriver.Firefox(options=options)  # or Chrome()
 
-    image_links = []
-    for result in img_results:
-        image_links.append(result["src"])
+    browser.get(search_url)
+    time.sleep(1)
+
+    image_links = get_image_links(browser, n_images)
 
     print(f"Found {len(image_links)} image" + ('s' if len(image_links) > 1 else ''))
 
+    browser.quit()
+
     if len(image_links) > 0:
         DATASET_FOLDER_IMAGES_DATA = DATASET_FOLDER_IMAGES + '/' + data + ('' if data.endswith('s') else 's')
-        if not os.path.exists(DATASET_FOLDER_IMAGES_DATA):
-            os.mkdir(DATASET_FOLDER_IMAGES_DATA)
+        make_dir(DATASET_FOLDER_IMAGES_DATA)
 
         print("Start downloading...")
 
         for i, image_link in enumerate(image_links):
-            response = requests.get(image_link)
-
             image_name = DATASET_FOLDER_IMAGES_DATA + '/' + str(i + 1) + ".jpg"
             with open(image_name, "wb") as file:
-                file.write(response.content)
+                file.write(requests.get(image_link).content)
 
-        print("Done !")
+    toc = time.perf_counter()
+    print(f"Done in {toc - tic:0.4f} seconds !")
+
+# https://www.geeksforgeeks.org/download-google-image-using-python-and-selenium/
+def get_image_links(browser, n_images):
+    img_results = browser.find_elements(by=By.CLASS_NAME, value="rg_i")
+
+    last_height = browser.execute_script("return document.body.scrollHeight")
+
+    while True:
+        browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        time.sleep(3)
+
+        new_height = browser.execute_script("return document.body.scrollHeight")
+
+        # click on "Show more results" (if exists)
+        try:
+            browser.find_element_by_css_selector(".YstHxe input").click()
+            time.sleep(3)
+        except:
+            pass
+
+        i = 0
+        for result in img_results:
+            image_link = result.get_attribute("src")
+            if image_link is not None and image_link.startswith("https"):
+                i += 1
+
+        if i >= n_images:
+            break
+
+        img_results = browser.find_elements(by=By.CLASS_NAME, value="rg_i")
+
+        # checking if we have reached the bottom of the page
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+    image_links = []
+    for result in browser.find_elements(by=By.CLASS_NAME, value="rg_i"):
+        image_link = result.get_attribute("src")
+
+        if image_link is not None and len(image_links) < n_images and image_link.startswith("https"):
+            image_links.append(image_link)
+
+    return image_links
 
 
 if __name__ == '__main__':
