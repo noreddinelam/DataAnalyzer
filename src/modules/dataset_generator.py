fb4cfb993f91f4f28a2a1cd878e2a7c2c9e2736b
Dalil CHABLIS <dalil.chablis@gmail.com>
2022-04-11 13:09:17 +0200

    wip: improve dataset generator

--- src/modules/dataset_generator.py
+++ src/modules/dataset_generator.py
@@ -8,71 +8,88 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
-GOOGLE_IMAGES_URL = "https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&"
+from src.modules.utils import make_dir
 
 DATASET_FOLDER = "../dataset"
 
+# TODO : refactor
+
 
 def main():
-    if os.path.exists(DATASET_FOLDER):
-        shutil.rmtree(DATASET_FOLDER)
-
     make_dir(DATASET_FOLDER)
-    download_google_images()
-
-
-def make_dir(path: str):
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-
-# https://selenium-python.readthedocs.io/getting-started.html
-# https://www.geeksforgeeks.org/download-google-image-using-python-and-selenium/
-def download_google_images():
-    DATASET_FOLDER_IMAGES = DATASET_FOLDER + "/images"
-    make_dir(DATASET_FOLDER_IMAGES)
 
     data = input("What are you looking for ? ")
     n_images = int(input("How many images do you want ? "))
 
-    print("Start searching...")
-    tic = time.perf_counter()
+    start = time.perf_counter()
+    start_generator(data, n_images)
+    end = time.perf_counter()
 
-    search_url = GOOGLE_IMAGES_URL + "q=" + data
+    print(f"Done in {end - start:0.4f} seconds !")
+
+
+def start_generator(data, n_images):
+    PEXELS_IMAGES_URL = "https://www.pexels.com/search/"
+    PIXABAY_IMAGES_URL = "https://pixabay.com/images/search/"
+    GOOGLE_IMAGES_URL = "https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&"
+
+    DATASET_FOLDER_IMAGES = DATASET_FOLDER + "/images"
+    make_dir(DATASET_FOLDER_IMAGES)
+
+    search_url = PEXELS_IMAGES_URL + data
+    pexels_search_images = search_images("Pexels", search_url, n_images)
+    pexels_search_images[0].quit()
+    pexels_image_links = pexels_search_images[1]
+    print(f"Found on Pexels : {len(pexels_image_links)} image" + ('s' if len(pexels_image_links) > 1 else ''))
+    image_links = pexels_image_links
+    current_n_images = n_images - len(pexels_image_links)
+
+    if len(image_links) < n_images:
+        search_url = PIXABAY_IMAGES_URL + data
+        pixabay_search_images = search_images("Pixabay", search_url, current_n_images)
+        pixabay_search_images[0].quit()
+        pixabay_image_links = pixabay_search_images[1]
+        print(f"Found on Pixabay : {len(pixabay_image_links)} image" + ('s' if len(pixabay_image_links) > 1 else ''))
+        image_links += pixabay_image_links
+        current_n_images = current_n_images - len(pixabay_image_links)
+
+        if len(image_links) < n_images:
+            search_url = GOOGLE_IMAGES_URL + "q=" + data
+            google_search_images = search_images("Google", search_url, current_n_images)
+            google_search_images[0].quit()
+            google_image_links = google_search_images[1]
+            print(f"Found on Google : {len(google_image_links)} image" + ('s' if len(google_image_links) > 1 else ''))
+            image_links += google_image_links
+
+    print(f"Found total {len(image_links)} image" + ('s' if len(image_links) > 1 else ''))
+
+    if len(image_links) > 0:
+        DATASET_FOLDER_IMAGES_DATA = DATASET_FOLDER_IMAGES + '/' + data + ('' if data.endswith('s') else 's')
+
+        if os.path.exists(DATASET_FOLDER_IMAGES_DATA):
+            shutil.rmtree(DATASET_FOLDER_IMAGES_DATA)
+        make_dir(DATASET_FOLDER_IMAGES_DATA)
+
+        download_images(image_links, DATASET_FOLDER_IMAGES_DATA)
+
+
+def search_images(on, search_url, n_images):
+    print("Start searching on " + on + "...")
 
     options = Options()
     options.headless = False
 
-    browser = webdriver.Firefox(options=options)  # or Chrome()
-
+    browser = webdriver.Firefox(options=options)
+    browser.maximize_window()
     browser.get(search_url)
-    time.sleep(1)
+    time.sleep(3)
 
-    image_links = get_image_links(browser, n_images)
+    return [browser, get_image_links(on, browser, n_images)]
 
-    print(f"Found {len(image_links)} image" + ('s' if len(image_links) > 1 else ''))
 
-    browser.quit()
-
-    if len(image_links) > 0:
-        DATASET_FOLDER_IMAGES_DATA = DATASET_FOLDER_IMAGES + '/' + data + ('' if data.endswith('s') else 's')
-        make_dir(DATASET_FOLDER_IMAGES_DATA)
-
-        print("Start downloading...")
-
-        for i, image_link in enumerate(image_links):
-            image_name = DATASET_FOLDER_IMAGES_DATA + '/' + str(i + 1) + ".jpg"
-            with open(image_name, "wb") as file:
-                file.write(requests.get(image_link).content)
-
-    toc = time.perf_counter()
-    print(f"Done in {toc - tic:0.4f} seconds !")
-
-# https://www.geeksforgeeks.org/download-google-image-using-python-and-selenium/
-def get_image_links(browser, n_images):
-    img_results = browser.find_elements(by=By.CLASS_NAME, value="rg_i")
-
+def get_image_links(on, browser, n_images):
     last_height = browser.execute_script("return document.body.scrollHeight")
+    img_results = []
 
     while True:
         browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
@@ -80,12 +97,32 @@ def get_image_links(browser, n_images):
 
         new_height = browser.execute_script("return document.body.scrollHeight")
 
-        # click on "Show more results" (if exists)
         try:
-            browser.find_element_by_css_selector(".YstHxe input").click()
+            className = ""
+            if on == "Pixabay":
+                className = ".button--2AOTE"
+            elif on == "Google":
+                className = ".YstHxe input"
+            browser.find_element_by_css_selector(className).click()
             time.sleep(3)
         except:
             pass
+
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+        className = ""
+
+        if on == "Pexels":
+            className = "MediaCard_image__ljFAl"
+        elif on == "Pixabay":
+            className = "link--h3bPW img"
+        elif on == "Google":
+            className = "rg_i"
+
+        img_results = browser.find_elements(by=By.CLASS_NAME, value=className)
 
         i = 0
         for result in img_results:
@@ -96,22 +133,22 @@ def get_image_links(browser, n_images):
         if i >= n_images:
             break
 
-        img_results = browser.find_elements(by=By.CLASS_NAME, value="rg_i")
-
-        # checking if we have reached the bottom of the page
-        if new_height == last_height:
-            break
-
-        last_height = new_height
-
     image_links = []
-    for result in browser.find_elements(by=By.CLASS_NAME, value="rg_i"):
+    for result in img_results:
         image_link = result.get_attribute("src")
 
         if image_link is not None and len(image_links) < n_images and image_link.startswith("https"):
             image_links.append(image_link)
 
     return image_links
+
+
+def download_images(image_links, folder):
+    print("Start downloading...")
+    for i, image_link in enumerate(image_links):
+        image_name = folder + '/' + str(i + 1) + ".jpg"
+        with open(image_name, "wb") as file:
+            file.write(requests.get(image_link).content)
 
 
 if __name__ == '__main__':
