363b0835dfe3b246cf5d757d978123c5934db0e3
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2022-05-31 17:32:57 +0200

    fix app cam and send image to api

--- src/modules/api/main.py
+++ src/modules/api/main.py
@@ -9,11 +9,9 @@ def root():
     return {"message": "Hello World"}
 
 @app.post("/upload_image")
-async def upload_image(file: UploadFile = File(...)):
-    with open(file.filename, "wb") as f:
-       print(file.filename)
+async def upload_image(image: UploadFile = File(...)):
 
-    return {"filename": file.filename}
+    return {"filename": image.filename}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, debug=True)
