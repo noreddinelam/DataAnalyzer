527ce50b08a734ca8e75c95b9159ac5f99f1bfe7
darko.djordjevic99@outlook.com <76133746+TheMandalorian-DD@users.noreply.github.com>
2022-06-05 17:27:06 +0200

    add changes

--- src/modules/mobile/mobile.py
+++ src/modules/mobile/mobile.py
@@ -52,7 +52,7 @@ kivy.require('2.0.0')
 
 class View(BoxLayout):
 
-    sound = SoundLoader.load('../data/src_kivy_garden_xcamera_data_shutter.wav')
+    sound = SoundLoader.load('./data/src_kivy_garden_xcamera_data_shutter.wav')
 
     def capture(self) -> None:
         '''
@@ -89,17 +89,6 @@ class View(BoxLayout):
 
 class Test(App):
     def build(self) -> View:
-        """dropdown = DropDown()
-        for index in range(10):
-            btn = Button(text='Value %d' % index, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-            dropdown.add_widget(btn)
-
-        mainbutton = Button(text='Hello', size_hint=(None, None))
-
-        mainbutton.bind(on_release=dropdown.open)
-
-        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))"""
         return View()
 
 
